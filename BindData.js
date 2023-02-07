bdf25aceaea58309244363d0596e5c42a1ab7a77
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-02-07 09:48:52 +0000

    Update BindData.js

--- BindData.js
+++ BindData.js
@@ -1,1 +1,17 @@
 /*  User should be able to type in any characters on input and those character should show in the browser. */
+
+
+// this is the code you are given: 
+
+function App() {
+  const [] = React.useState("");
+
+  return (
+    <>
+      <input type="text" placeholder="Enter Text" />
+      <p></p>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
