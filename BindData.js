8d5493b2973da1d9953e814739fb3ee31aef1d3c
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-02-07 09:57:52 +0000

    solved

--- BindData.js
+++ BindData.js
@@ -3,15 +3,38 @@
 
 // this is the code you are given: 
 
-function App() {
-  const [] = React.useState("");
 
+function App() {
+  const [ text, setText ] = React.useState("");
+  const handleInput = (e) => {
+    setText(e.target.value)
+  }
   return (
     <>
-      <input type="text" placeholder="Enter Text" />
-      <p></p>
+      <input value={text} onChange={handleInput} type="text" placeholder="Enter Text" />
+      <p>{text}</p>
     </>
   );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// although I would have wrote it all like this: 
+
+import { useState } from "react"
+
+const App = () => {
+  const [ text, setText ] = useState("")
+  const handleInput = (e) => {
+    setText(e.target.value}  
+  }
+  return (
+    <>
+       <input value={text} onChange={handleInput} type="text" placeholder="Enter Text"/>
+       <p>{text}</p>
+    </>
+    )}
+}
+
+export default App;
