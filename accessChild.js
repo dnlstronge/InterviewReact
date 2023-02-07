820cead07e5187e66e3378a3d1d34cd20dec41c5
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-02-07 13:58:01 +0000

    Create accessChild.js

---
+++ accessChild.js
@@ -0,0 +1,31 @@
+/**
+  Challenge: Show entire Child component content inside Parent component. Only add code on Parent Component below
+  
+  Solution: https://codepen.io/angelo_jin/pen/MWEVJNb
+  Video for Reference: https://youtu.be/VzNNjNmbXpY
+  
+You are given the following code 
+
+**/
+function Child() {
+  return <div>This is children content</div>;
+}
+
+// Add code only here
+function Parent() {
+  return (
+    <div>
+      <h3>Parent Component</h3>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Parent>
+      <Child />
+    </Parent>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
