60089073138b648b7b7cd9798162b4ad59e718b9
Dan <114242578+dnlstronge@users.noreply.github.com>
2023-02-07 09:43:03 +0000

    Create hideshow.js

---
+++ hideshow.js
@@ -0,0 +1,18 @@
+/* Clicking the button should hide the string 
+*/
+
+          import React from "react"
+
+
+          const App = () => {
+              return (
+                  <div>
+                      <button>Click to hide</button>
+                      <p>HIDE ME</p>
+                  </div>
+              )
+          }
+
+          export default App;
+
+          const App = ()
